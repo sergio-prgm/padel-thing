9528db8b0f9a014925df526fd3bd73dbda6f5366
Sergio Esteve <sergio-prgm@outlook.com>
2023-06-27 01:02:07 +0200

    buildlaskdfd

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
 import solid from "solid-start/vite";
+import vercel from "solid-start-vercel"
 
 import { defineConfig } from "vite";
 export default defineConfig({
   plugins: [
     solid({
       ssr: false,
+    }),
+    vercel({
+      edge: false
     })
   ],
 });
