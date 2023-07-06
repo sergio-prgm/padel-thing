ae1d3a07738677542031527db2495f1e24140b8f
Sergio Esteve <sergio-prgm@outlook.com>
2023-07-07 00:03:18 +0200

    Feat: track games won (only std)

--- src/routes/game.jsx
+++ src/routes/game.jsx
@@ -30,6 +30,7 @@ function Score({ teams }) {
   const [scoreA, setScoreA] = createSignal(0)
   const [scoreB, setScoreB] = createSignal(0)
   const [result, setResult] = createSignal([0, 0])
+  const [games, setGames] = createSignal([0, 0])
   const [isServing, setIsServing] = createSignal(0)
 
   const tennisTranslate = (valueA, valueB) => {
@@ -56,6 +57,18 @@ function Score({ teams }) {
     console.log(server())
     console.log(isServing())
   })
+
+  createEffect(() => {
+    if (result()[0] >= 6) {
+      setGames([games()[0] + 1, games()[1]])
+      setResult([0, 0])
+    } else if (result()[1] >= 6) {
+      setGames([games()[0], games()[1] + 1])
+      setResult([0, 0])
+    }
+  })
+
+  createEffect(() => console.log(games()))
 
   createEffect(() => {
     if (scoreA() - scoreB() >= 2 && scoreB() >= 3) {
@@ -88,19 +101,25 @@ function Score({ teams }) {
 
   return (
     <div>
-      <div class="flex gap-6 w-fit mx-auto py-6">
+      <div class="flex gap-6 w-fit mx-auto pt-6">
+        {games().map(v => (
+          <span class="px-6 py-4 font-black text-3xl font-mono border-2 border-blue-800 text-gray-800 ">{v}</span>
+        ))}
+      </div>
+      <div class="flex gap-6 w-fit mx-auto pb-6">
         {result().map(v => (
-          <span class="px-6 py-4 font-black text-3xl font-mono bg-blue-800 text-gray-50 ">{v}</span>
-        ))}</div>
+          <span class="px-6 py-4 font-black text-3xl font-mono bg-blue-800 border-2 border-blue-800 text-gray-50 ">{v}</span>
+        ))}
+      </div>
 
       <div class="flex mx-auto w-fit gap-6">
         <Btn handler={handleClickA}>
-          <span class="block border-b border-gray-400">{playerA1.name || 'Player A1'}{server() === playerA1.i && '*'}</span>
-          <span class="block">{playerA2.name || 'Player A2'}{server() === playerA2.i && '*'}</span>
+          <Player name={playerA1.name} isServing={() => server() === playerA1.i} isTop={true} />
+          <Player name={playerA2.name} isServing={() => server() === playerA2.i} />
         </Btn>
         <Btn handler={handleClickB}>
-          <span class="block border-b border-gray-400">{playerB1.name || 'Player B1'}{server() === playerB1.i && '*'}</span>
-          <span class="block">{playerB2.name || 'Player B2'}{server() === playerB2.i && '*'}</span>
+          <Player name={playerB1.name} isServing={() => server() === playerB1.i} isTop={true} />
+          <Player name={playerB2.name} isServing={() => server() === playerB2.i} />
         </Btn>
       </div>
 
@@ -109,9 +128,9 @@ function Score({ teams }) {
   )
 }
 
-// function Player({ name, isServing }) {
-//   return <span>{name}</span>
-// }
+function Player({ name, isServing, isTop }) {
+  return <span class={`block ${isTop && "border-b border-gray-400"} ${isServing() && "text-blue-500"}`}>{name}{isServing() && '*'}</span>
+}
 
 function Btn({ children, handler }) {
   return <button onclick={handler} class="font-bold border-2 px-4 py-2 border-blue-500 hover:bg-blue-100 text-2xl">{children}</button>
